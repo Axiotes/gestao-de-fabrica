fc893a578adb967fbe12c9547b891c9e40256bbe
Axiotes <142627032+Axiotes@users.noreply.github.com>
2024-02-01 12:55:57 -0300

    Add files via upload
    Arquivo api

--- main.py
+++ main.py
@@ -2,27 +2,23 @@ from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
 import mysql.connector
 
-
 app = FastAPI()
-
-origins = [
-    "http://localhost"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 def connect():
     return mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='',
-        database='projeto'
+        host="localhost",
+        port="3306",
+        user="root",
+        password="mysql",
+        database="projeto"
     )
 
 @app.post("/login")
@@ -30,14 +26,10 @@ async def validate_login(
     email: str = Form(...),
     senha: str = Form(...)
 ):
-    try:
-        comando = f"""SELECT * FROM users WHERE email = '{email}' and senha = md5('{senha}')"""
-        conn = connect()
-        cursor = conn.cursor()
-        data = cursor.execute(comando)
-        cursor.close()
+    conn = connect()
+    cursor = conn.cursor()
+    comando = f"""SELECT * FROM users WHERE email = '{email}' and senha = md5('{senha}')"""
+    data = cursor.execute(comando)
+    #records = cursor.fetchall()
 
-        return data.fetchone()
-    except mysql.connector.Error as err:
-
-        return format(err)
+    return data
